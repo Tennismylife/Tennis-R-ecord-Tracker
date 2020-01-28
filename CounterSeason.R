ee7ec04f4d586dd4cd16fd5583a549c5e0190f1c
Tennismylife <infotennismylife@gmail.com>
2020-01-28 20:13:12 +0100

    Update

--- CounterSeason.R
+++ CounterSeason.R
@@ -2,44 +2,26 @@ library(NLP)
 
 
 CountRoundSeason <- function() {
+  dbm <- db
   
   ## drop walkover matches (not countable)
-  db <- db[!db$score=="W/O" & !db$score=="DEF" & !db$score=="(ABN)"]
+  dbm <- dbm[!dbm$score=="W/O" & !dbm$score=="DEF" & !dbm$score=="(ABN)"]
   
   ##SelectRound
-  db <- db[tourney_level == 'G']
+  dbm <- dbm[tourney_level == 'G']
   
-  #db <- db[round == 'R16']
+  dbm <- dbm[round == 'QF']
   
   #dbm <- dbm[winner_ioc =='ITA']
-    
-  wins <- db[,c('winner_name','tourney_id', 'score')]
   
-  wins <- wins[lengths(regmatches(wins$score, gregexpr("-", wins$score))) == '5']
-  
-  losses <- db[,c('loser_name','tourney_id', 'score')]
-  
-  losses <- losses[lengths(regmatches(losses$score, gregexpr("-", losses$score))) == '5']
-  
-  names(wins)[1] <- names(losses)[1] <- "name"
-  
-  matches <- union(wins, losses)
+  wins <- dbm[,c('winner_name','tourney_id')]
   
   #extract year from tourney_date
-  matches$tourney_id <- stringr::str_sub(matches$tourney_id, 0 ,4)
+  wins$tourney_id <- stringr::str_sub(wins$tourney_id, 0 ,4)
   
-  names(matches)[2] <- "year"
+  names(wins)[2] <- "year"
   
-  #matches <- matches[year == '2020']
-  
-  print(matches)
-  
-  season <- matches[, .N, by = list(matches$name, matches$year)]
-  
-  count <- matches[, .N, by = matches$name]
-  count <- count[order(-N)]
-  
-  print(season)
+  season <- wins[, .N, by = list(wins$winner_name, wins$year)]
   
   names(season)[1] <- "player"
   names(season)[2] <- "year"
@@ -49,25 +31,25 @@ CountRoundSeason <- function() {
   
   ## order by decreasing
   season <- season[order(-N)]
- 
+  
   season <- season[order(year)]
   
   out <- aggregate(season$year ~ season$player, season, c)
- 
+  
   names(out)[1] <- "player"
   names(out)[2] <- "seasons"
-
+  
   nn <- sapply(strsplit(as.character(out$seasons), ", "), length)
   
   mm <- as.data.frame(cbind(out, n=nn))
   
   mm <- mm[order(mm$n, decreasing=TRUE), ]
-
+  
   mm$seasons <-  as.character(mm$seasons)
   
   #select 1st 20
   mm <- mm[1:40,]
   
-  print(count)
+  print(mm)
   
 }
