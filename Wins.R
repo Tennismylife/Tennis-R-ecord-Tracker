9dbec7b61cde837cd07ec34a215830b1dcaaf48b
Tennismylife <infotennismylife@gmail.com>
2020-03-08 20:45:21 +0100

    Fix

--- Wins.R
+++ Wins.R
@@ -11,25 +11,35 @@ WinsCategory <- function(category) {
   
   ## drop walkover matches (not countable)
   dbm <- dbm[!dbm$score=="W/O" & !dbm$score=="DEF" & !dbm$score=="(ABN)"]
+  
   ## count occurrences of won matches
   res <- dbm[,.N, by=winner_name]
+  
   ## order by decreasing
   setorder(res, -N, na.last=FALSE)
+  
   res <- res[1:20,]
+  
   print(res)
 }
 
 
 WinsTour <- function(tournament, tournament2, tournament3) {
+  
   ## only select tournaments in the previously defined pool
   dbm <- db[tourney_name == tournament | tourney_name == tournament2 | tourney_name == tournament3]
+  
   ## drop walkover matches (not countable)
   dbm <- dbm[!dbm$score=="W/O" & !dbm$score=="DEF" & !dbm$score=="(ABN)"]
+  
   ## count occurrences of won matches
   res <- dbm[,.N, by=winner_name]
+  
   ## order by decreasing
   setorder(res, -N, na.last=FALSE)
+  
   res <- res[1:20,]
+  
   print(res)
 }
 
@@ -52,14 +62,17 @@ winsSurface <- function(court) {
 }
 
 WinsOverall <- function() {
-  dbm <- db
   ## drop walkover matches (not countable)
-  dbm <- dbm[!dbm$score=="W/O" & !dbm$score=="DEF" & !dbm$score=="(ABN)"]
+  db <- db[!db$score=="W/O" & !db$score=="DEF" & !db$score=="(ABN)"]
+  
   ## count occurrences of won matches
-  res <- dbm[,.N, by=winner_name]
+  res <- db[,.N, by=winner_name]
+  
   ## order by decreasing
   setorder(res, -N, na.last=FALSE)
+  
   res <- res[1:20,]
+  
   print(res)
   
 }
@@ -68,10 +81,14 @@ LossesOverall <- function() {
   dbm <- db
   ## drop walkover matches (not countable)
   dbm <- dbm[!dbm$score=="W/O" & !dbm$score=="DEF" & !dbm$score=="(ABN)"]
+  
   ## count occurrences of lost matches
   out <- dbm[,.N, by=loser_name]
+  
   ## order by decreasing
   setorder(out, -N, na.last=FALSE)
+  
   res <- res[1:100,]
+  
   print(out)
 }
